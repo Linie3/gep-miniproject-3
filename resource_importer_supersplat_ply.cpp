986deb6d312d644646ecfa95c992d5db5c61a8dd
Linus <lnia2004@web.de>
2026-06-29 22:29:06 +0200

    State

--- resource_importer_supersplat_ply.cpp
+++ resource_importer_supersplat_ply.cpp
@@ -35,6 +35,7 @@ void ResourceImporterSupersplatPly::get_import_options(const String &p_path, Lis
     int p_preset) const {
 	r_options->push_back(ImportOption(PropertyInfo(Variant::INT, "instance_count_cap"), -1));
 	r_options->push_back(ImportOption(PropertyInfo(Variant::FLOAT, "upscale"), 1.0f));
+	r_options->push_back(ImportOption(PropertyInfo(Variant::STRING_NAME, "data_format", PROPERTY_HINT_ENUM, "Standard,SuperSplat Compressed"), "Standard"));
 }
 
 bool ResourceImporterSupersplatPly::get_option_visibility(const String &p_path, const String &p_option,
@@ -51,129 +52,137 @@ Error ResourceImporterSupersplatPly::import(ResourceUID::ID p_source_id, const S
 
     std::unique_ptr<happly::PLYData> plyIn = std::make_unique<happly::PLYData>(file->get_path_absolute().utf8().get_data());
 
-	ERR_FAIL_COND_V_MSG(plyIn->inputDataFormat != happly::DataFormat::Binary, ERR_FILE_UNRECOGNIZED, "PLY file does not contain binary little endian data.");
-	ERR_FAIL_COND_V_MSG(!plyIn->hasElement("vertex"), ERR_FILE_UNRECOGNIZED, "PLY file does not contain vertices.");
-    happly::Element& vertices = plyIn->getElement("vertex");
-	ERR_FAIL_COND_V_MSG(!vertices.hasProperty("x"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain position information.");
-	ERR_FAIL_COND_V_MSG(!vertices.hasProperty("f_dc_0"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain primary color information.");
-	ERR_FAIL_COND_V_MSG(!vertices.hasProperty("scale_0"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain scale information.");
-	ERR_FAIL_COND_V_MSG(!vertices.hasProperty("rot_0"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain rotation information.");
-	ERR_FAIL_COND_V_MSG(!vertices.hasProperty("f_rest_0"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain spherical harmonics information.");
-	ERR_FAIL_COND_V_MSG(!vertices.hasProperty("opacity"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain opacity information.");
+	const String format = p_options["data_format"];
 
-    int instance_count = vertices.count;
-	char position_begin_idx = vertices.getPropertyPtr("x")->position;
-	char rotation_begin_idx = vertices.getPropertyPtr("rot_0")->position;
-	char scale_begin_idx = vertices.getPropertyPtr("scale_0")->position;
-	char color_begin_idx = vertices.getPropertyPtr("f_dc_0")->position;
-	char opacity_idx = vertices.getPropertyPtr("opacity")->position;
-	char spherical_harmonics_begin_idx = vertices.getPropertyPtr("f_rest_0")->position;
+	if (format == "Standard") {
 
-	if (const int instance_count_cap = p_options["instance_count_cap"]; instance_count_cap >= 0) {
-		instance_count = std::min(instance_count, instance_count_cap);
-	}
+		ERR_FAIL_COND_V_MSG(plyIn->inputDataFormat != happly::DataFormat::Binary, ERR_FILE_UNRECOGNIZED, "PLY file does not contain binary little endian data.");
+		ERR_FAIL_COND_V_MSG(!plyIn->hasElement("vertex"), ERR_FILE_UNRECOGNIZED, "PLY file does not contain vertices.");
+	    happly::Element& vertices = plyIn->getElement("vertex");
+		ERR_FAIL_COND_V_MSG(!vertices.hasProperty("x"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain position information.");
+		ERR_FAIL_COND_V_MSG(!vertices.hasProperty("f_dc_0"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain primary color information.");
+		ERR_FAIL_COND_V_MSG(!vertices.hasProperty("scale_0"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain scale information.");
+		ERR_FAIL_COND_V_MSG(!vertices.hasProperty("rot_0"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain rotation information.");
+		ERR_FAIL_COND_V_MSG(!vertices.hasProperty("f_rest_0"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain spherical harmonics information.");
+		ERR_FAIL_COND_V_MSG(!vertices.hasProperty("opacity"), ERR_FILE_UNRECOGNIZED, "Vertices do not contain opacity information.");
 
-	const float upscale = p_options["upscale"];
+	    int instance_count = vertices.count;
+		char position_begin_idx = vertices.getPropertyPtr("x")->position;
+		char rotation_begin_idx = vertices.getPropertyPtr("rot_0")->position;
+		char scale_begin_idx = vertices.getPropertyPtr("scale_0")->position;
+		char color_begin_idx = vertices.getPropertyPtr("f_dc_0")->position;
+		char opacity_idx = vertices.getPropertyPtr("opacity")->position;
+		char spherical_harmonics_begin_idx = vertices.getPropertyPtr("f_rest_0")->position;
 
-	constexpr int floats_per_coefficient = 3; //3-dimensional coefficients
-	constexpr int floats_per_pixel = 3; //RGBF texture
-    constexpr int pixel_per_coefficient = floats_per_coefficient / floats_per_pixel;
-    constexpr int num_coefficients = 3 + 5 + 7; // First 3 order of spherical harmonials
-    constexpr int pixels_per_splat_sh = num_coefficients * pixel_per_coefficient;
-    constexpr int bytes_per_pixel = floats_per_pixel * sizeof(float);
-
-	RenderingDevice *rendering_device = RenderingServer::get_singleton()->get_rendering_device();
-
-	const int max_pixels_per_dimension = rendering_device->limit_get(RenderingDevice::LIMIT_MAX_TEXTURE_SIZE_2D);
-	const int max_coefficients_per_row = max_pixels_per_dimension / pixel_per_coefficient;
-	const int max_splat_sh_per_row = max_coefficients_per_row / num_coefficients;
-	const int max_splat_sh = max_pixels_per_dimension * max_splat_sh_per_row;
-
-	if (max_splat_sh < instance_count) {
-		WARN_PRINT(("Too many splats in model, discarded last " + std::to_string(instance_count - max_splat_sh) + " splats").c_str());
-		instance_count = instance_count - max_splat_sh;
-	}
-
-
-    Ref<MultiMesh> multi_mesh;
-    multi_mesh.instantiate();
-    multi_mesh->set_transform_format(MultiMesh::TRANSFORM_3D);
-    multi_mesh->set_instance_count(0);
-    multi_mesh->set_use_colors(true);
-    multi_mesh->set_use_custom_data(true);
-    multi_mesh->set_instance_count(instance_count);
-
-	//const int splat_sh_size_bytes = instance_count * bytes_per_splat_sh;
-	const int splat_sh_size_pixels = instance_count * pixels_per_splat_sh;
-	const int min_pixels_per_row = std::ceil(std::sqrt(static_cast<double>(instance_count * pixels_per_splat_sh)));
-	const int pixels_per_row = ((min_pixels_per_row + pixels_per_splat_sh - 1) / pixels_per_splat_sh) * pixels_per_splat_sh;
-	const int pixels_per_column = std::ceil(static_cast<double>(splat_sh_size_pixels) / pixels_per_row);
-
-    Vector<uint8_t> p_data;
-    p_data.resize(pixels_per_row * pixels_per_column * bytes_per_pixel); // Reserve whole cube size, including empty buffer after last sh
-    uint8_t *sh_texture_dest = p_data.ptrw();
-
-    constexpr int num_floats_per_splat = num_coefficients * floats_per_coefficient + 14;
-    Vector<float> splat_buf;
-    splat_buf.resize(num_floats_per_splat);
-
-	AABB total_aabb;
-	bool first_aabb = true;
-
-	String line;
-	while (line != "end_header") {
-		line = file->get_line();
-	}
-
-	for (int i = 0; i < instance_count; ++i) {
-	    file->get_buffer(reinterpret_cast<uint8_t *>(splat_buf.ptrw()), num_floats_per_splat * sizeof(float));
-	    const float* raw = splat_buf.ptr();
-
-	    Transform3D transform;
-	    transform.origin = Vector3(raw[position_begin_idx], raw[position_begin_idx + 1], raw[position_begin_idx + 2]) * upscale;
-
-		const Quaternion rot_quat(raw[rotation_begin_idx + 1], raw[rotation_begin_idx + 2], raw[rotation_begin_idx + 3], raw[rotation_begin_idx]);
-
-		Basis basis(rot_quat);
-	    transform.basis = basis;
-
-		Vector3 scale = Vector3(std::exp(raw[scale_begin_idx]), std::exp(raw[scale_begin_idx + 1]), std::exp(raw[scale_begin_idx + 2])) * upscale;
-
-		if (first_aabb) {
-			total_aabb = AABB(transform.origin, Vector3(0.01, 0.01, 0.01));
-			first_aabb = false;
-		} else {
-			total_aabb.expand_to(transform.origin);
+		if (const int instance_count_cap = p_options["instance_count_cap"]; instance_count_cap >= 0) {
+			instance_count = std::min(instance_count, instance_count_cap);
 		}
 
-	    multi_mesh->set_instance_color(i, Color(raw[color_begin_idx], raw[color_begin_idx + 1], raw[color_begin_idx + 2], 1.0f / (1.0f + std::exp(-raw[opacity_idx]))));
-	    multi_mesh->set_instance_transform(i, transform);
-	    multi_mesh->set_instance_custom_data(i, Color(scale.x, scale.y, scale.z, i));
+		const float upscale = p_options["upscale"];
 
-	    const auto current_row_sh = reinterpret_cast<float *>(sh_texture_dest + i * num_coefficients * bytes_per_pixel);
-	    memcpy(current_row_sh, &raw[spherical_harmonics_begin_idx], 45 * sizeof(float));
+		constexpr int floats_per_coefficient = 3; //3-dimensional coefficients
+		constexpr int floats_per_pixel = 3; //RGBF texture
+	    constexpr int pixel_per_coefficient = floats_per_coefficient / floats_per_pixel;
+	    constexpr int num_coefficients = 3 + 5 + 7; // First 3 order of spherical harmonials
+	    constexpr int pixels_per_splat_sh = num_coefficients * pixel_per_coefficient;
+	    constexpr int bytes_per_pixel = floats_per_pixel * sizeof(float);
+
+		RenderingDevice *rendering_device = RenderingServer::get_singleton()->get_rendering_device();
+
+		const int max_pixels_per_dimension = rendering_device->limit_get(RenderingDevice::LIMIT_MAX_TEXTURE_SIZE_2D);
+		const int max_coefficients_per_row = max_pixels_per_dimension / pixel_per_coefficient;
+		const int max_splat_sh_per_row = max_coefficients_per_row / num_coefficients;
+		const int max_splat_sh = max_pixels_per_dimension * max_splat_sh_per_row;
+
+		if (max_splat_sh < instance_count) {
+			WARN_PRINT(("Too many splats in model, discarded last " + std::to_string(instance_count - max_splat_sh) + " splats").c_str());
+			instance_count = instance_count - max_splat_sh;
+		}
+
+
+	    Ref<MultiMesh> multi_mesh;
+	    multi_mesh.instantiate();
+	    multi_mesh->set_transform_format(MultiMesh::TRANSFORM_3D);
+	    multi_mesh->set_instance_count(0);
+	    multi_mesh->set_use_colors(true);
+	    multi_mesh->set_use_custom_data(true);
+	    multi_mesh->set_instance_count(instance_count);
+
+		//const int splat_sh_size_bytes = instance_count * bytes_per_splat_sh;
+		const int splat_sh_size_pixels = instance_count * pixels_per_splat_sh;
+		const int min_pixels_per_row = std::ceil(std::sqrt(static_cast<double>(instance_count * pixels_per_splat_sh)));
+		const int pixels_per_row = ((min_pixels_per_row + pixels_per_splat_sh - 1) / pixels_per_splat_sh) * pixels_per_splat_sh;
+		const int pixels_per_column = std::ceil(static_cast<double>(splat_sh_size_pixels) / pixels_per_row);
+
+	    Vector<uint8_t> p_data;
+	    p_data.resize(pixels_per_row * pixels_per_column * bytes_per_pixel); // Reserve whole cube size, including empty buffer after last sh
+	    uint8_t *sh_texture_dest = p_data.ptrw();
+
+	    constexpr int num_floats_per_splat = num_coefficients * floats_per_coefficient + 14;
+	    Vector<float> splat_buf;
+	    splat_buf.resize(num_floats_per_splat);
+
+		AABB total_aabb;
+		bool first_aabb = true;
+
+		String line;
+		while (line != "end_header") {
+			line = file->get_line();
+		}
+
+		for (int i = 0; i < instance_count; ++i) {
+		    file->get_buffer(reinterpret_cast<uint8_t *>(splat_buf.ptrw()), num_floats_per_splat * sizeof(float));
+		    const float* raw = splat_buf.ptr();
+
+		    Transform3D transform;
+		    transform.origin = Vector3(raw[position_begin_idx], raw[position_begin_idx + 1], raw[position_begin_idx + 2]) * upscale;
+
+			const Quaternion rot_quat(raw[rotation_begin_idx + 1], raw[rotation_begin_idx + 2], raw[rotation_begin_idx + 3], raw[rotation_begin_idx]);
+
+			Basis basis(rot_quat);
+		    transform.basis = basis;
+
+			Vector3 scale = Vector3(std::exp(raw[scale_begin_idx]), std::exp(raw[scale_begin_idx + 1]), std::exp(raw[scale_begin_idx + 2])) * upscale;
+
+			if (first_aabb) {
+				total_aabb = AABB(transform.origin, Vector3(0.01, 0.01, 0.01));
+				first_aabb = false;
+			} else {
+				total_aabb.expand_to(transform.origin);
+			}
+
+		    multi_mesh->set_instance_color(i, Color(raw[color_begin_idx], raw[color_begin_idx + 1], raw[color_begin_idx + 2], 1.0f / (1.0f + std::exp(-raw[opacity_idx]))));
+		    multi_mesh->set_instance_transform(i, transform);
+		    multi_mesh->set_instance_custom_data(i, Color(scale.x, scale.y, scale.z, i));
+
+		    const auto current_row_sh = reinterpret_cast<float *>(sh_texture_dest + i * num_coefficients * bytes_per_pixel);
+		    memcpy(current_row_sh, &raw[spherical_harmonics_begin_idx], 45 * sizeof(float));
+		}
+
+		multi_mesh->set_custom_aabb(total_aabb);
+
+		const Ref<Image> image = Image::create_from_data(pixels_per_row, pixels_per_column, false, Image::FORMAT_RGBF, p_data);
+
+	    Ref<ImageTexture> texture;
+	    texture.instantiate();
+	    texture->set_image(image);
+	    material->set_shader_parameter("sh_tex", texture);
+
+	    Ref<QuadMesh> mesh;
+	    mesh.instantiate();
+	    mesh->set_size(Vector2(2.0f, 2.0f));
+	    mesh->set_material(material);
+
+	    multi_mesh->set_mesh(mesh);
+
+	    const Error err = ResourceSaver::save(multi_mesh, p_save_path + ".multimesh", 0);
+	    ERR_FAIL_COND_V_MSG(err != OK, err, "Cannot save supersplat multimesh to file \"" + p_save_path + ".res\".");
+
+	    return OK;
+
 	}
 
-	multi_mesh->set_custom_aabb(total_aabb);
-
-	const Ref<Image> image = Image::create_from_data(pixels_per_row, pixels_per_column, false, Image::FORMAT_RGBF, p_data);
-
-    Ref<ImageTexture> texture;
-    texture.instantiate();
-    texture->set_image(image);
-    material->set_shader_parameter("sh_tex", texture);
-
-    Ref<QuadMesh> mesh;
-    mesh.instantiate();
-    mesh->set_size(Vector2(2.0f, 2.0f));
-    mesh->set_material(material);
-
-    multi_mesh->set_mesh(mesh);
-
-    const Error err = ResourceSaver::save(multi_mesh, p_save_path + ".multimesh", 0);
-    ERR_FAIL_COND_V_MSG(err != OK, err, "Cannot save supersplat multimesh to file \"" + p_save_path + ".res\".");
-
-    return OK;
+	return FAILED;
 }
 
 ResourceImporterSupersplatPly::ResourceImporterSupersplatPly() {
